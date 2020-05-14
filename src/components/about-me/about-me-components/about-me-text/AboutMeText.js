fe46a42df72a69fd7ed2895234839eee47e27e2c
PinkFished <d.macias.apps@gmail.com>
2020-05-13 22:50:29 -0700

    Fix class to className

--- src/components/about-me/about-me-components/about-me-text/AboutMeText.js
+++ src/components/about-me/about-me-components/about-me-text/AboutMeText.js
@@ -4,7 +4,7 @@ const AboutMeText = () => {
   return (
     <div>
       <h1 className="about-me-header">I'm Daniel</h1>
-      <p class="about-me-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sapiente consequuntur non fugit eius porro fuga. Odio minima, accusamus esse quam id mollitia illum eum, aperiam vitae voluptas consequatur ratione alias laudantium corporis, eaque labore recusandae aliquid ullam iusto quae sunt numquam cupiditate optio. Nihil vero quos, consectetur ea voluptates sit dolores maxime tempore, cupiditate quidem voluptas quisquam autem impedit cum, incidunt libero minus dignissimos aliquam officiis aliquid corrupti facere doloribus dolorem odit! Id, repudiandae doloremque! Qui perspiciatis consequuntur quas suscipit, cupiditate quia, ex illum nemo blanditiis necessitatibus iusto doloribus voluptas, consectetur ducimus dolore doloremque esse? Quibusdam natus tempora nisi!</p>
+      <p className="about-me-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sapiente consequuntur non fugit eius porro fuga. Odio minima, accusamus esse quam id mollitia illum eum, aperiam vitae voluptas consequatur ratione alias laudantium corporis, eaque labore recusandae aliquid ullam iusto quae sunt numquam cupiditate optio. Nihil vero quos, consectetur ea voluptates sit dolores maxime tempore, cupiditate quidem voluptas quisquam autem impedit cum, incidunt libero minus dignissimos aliquam officiis aliquid corrupti facere doloribus dolorem odit! Id, repudiandae doloremque! Qui perspiciatis consequuntur quas suscipit, cupiditate quia, ex illum nemo blanditiis necessitatibus iusto doloribus voluptas, consectetur ducimus dolore doloremque esse? Quibusdam natus tempora nisi!</p>
     </div>)
 }
 
