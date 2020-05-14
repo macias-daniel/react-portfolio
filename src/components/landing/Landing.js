d9e3194c5c448a8de502fb5df05a13d2ac7fc989
PinkFished <d.macias.apps@gmail.com>
2020-05-13 22:12:20 -0700

    Added about me down button

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./Landing.css"
 import Title from "./landing-components/title/Title"
+import PageDown from "./landing-components/pageDown/PageDown"
 
 
 const Landing = () => {
   return (
     <div className="landingContainer">
       <Title />
+      <PageDown />
     </div>
   )
 }
