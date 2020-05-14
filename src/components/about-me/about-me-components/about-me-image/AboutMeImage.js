db7034da8971b178f9628e7cd62831b253e20272
PinkFished <d.macias.apps@gmail.com>
2020-05-13 23:38:24 -0700

    Updates

--- src/components/about-me/about-me-components/about-me-image/AboutMeImage.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./AboutMeImage.css"
-import profileImage from "../../../../assets/profilePhoto.jpg"
-
-const AboutMeImage = () => {
-  return (<img className="about-me-img" src={profileImage} alt="Daniel" />)
-}
-
-export default AboutMeImage
