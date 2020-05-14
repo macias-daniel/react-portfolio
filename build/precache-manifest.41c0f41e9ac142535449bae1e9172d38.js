3c6512b03553ed6b5e160c0ed125f901f1797058
PinkFished <d.macias.apps@gmail.com>
2020-05-14 00:40:14 -0700

    Add build folder

---
+++ build/precache-manifest.41c0f41e9ac142535449bae1e9172d38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c5d24bacb2f739d53e5c0327e3c90a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "63c0b2f0d2240f40717d",
+    "url": "/react-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3f689151f881fe141f6a",
+    "url": "/react-portfolio/static/css/main.ef4b46c0.chunk.css"
+  },
+  {
+    "revision": "63c0b2f0d2240f40717d",
+    "url": "/react-portfolio/static/js/2.e474b01f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.e474b01f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f689151f881fe141f6a",
+    "url": "/react-portfolio/static/js/main.c900967a.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  },
+  {
+    "revision": "26efa5a6f2fbc9a7fe1ff11e5aa40e7c",
+    "url": "/react-portfolio/static/media/profilePhoto.26efa5a6.jpg"
+  }
+]);
